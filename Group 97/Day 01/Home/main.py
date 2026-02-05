af89300386eae2385db6d488024d0068993ed737
TamariMargiani <tamarimargiani@gmail.com>
2026-02-05 23:55:21 +0400

    second commit

---
+++ Group 97/Day 01/Home/main.py
@@ -0,0 +1,45 @@
+# HOMEWORK
+
+# 1. Sololearn-ზე რეგისტრაცია  +
+
+# 2. Sololearn-ის დავალება + 
+
+# 3. შევქმნათ 5 ცვლადი საკუთარ თავზე 
+
+name = 'Tamari'
+lastname = 'Margiani'
+age = 32
+academy = 'GOA'
+proffesion = 'student'
+
+print(name, lastname, age, academy, proffesion)
+
+print(name + ' ' + lastname + ' ' + str('32') + ' ' + academy + ' ' + proffesion )
+
+
+# 4. მათემატიკური ოპერატორები 
+
+print(5 + 5)
+print(23 -9)
+print(67 * 15)
+print(90 / 4)
+print(90 // 4)
+
+
+# 5. 5 მაგალითი Type() მეთოდზე
+
+name = 'Tamuna'
+age = 25
+experience = 4.5
+proffesion = 'web developer'
+x = '57'
+
+print(type(name))
+print(type(age))
+print(type(experience))
+print(type(proffesion))
+print(type(x))
+
+
+
+
