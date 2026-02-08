52b0101263c5975a1a3abdce96f2248e145bd1f1
TamariMargiani <tamarimargiani@gmail.com>
2026-02-08 06:35:28 +0400

    third commit

---
+++ Group 97/Day 02/Class/main.py
@@ -0,0 +1,82 @@
+# ცვლადი
+
+# x = 100
+# y = 200
+# z = y
+# b = y // 200
+# c = x + y
+# d = y / x
+
+
+# print(z)
+# print(c)
+# print(d)
+# print(b)
+
+
+# song = "patriki"
+# song = "let it be"
+
+# print(song)
+
+# case sensitive 
+
+# credit = 100
+# Credit = 200
+# CREDIT = 300
+
+# print(credit)
+# print(Credit)
+# print(CREDIT)
+
+
+# ზუსტად უნდა გადავცეთ პრინტს ცვლადის სახელწოდება თუ გვინდა კონკრეტულად ის გამოიტანოს რა მნიშვნელობაც ცვლადის სახელს შეესაბამება
+
+# budget = 200
+# print('budget')
+# print(budget)
+
+
+# price = 139
+# print(price)
+
+
+# print(24 * 60 * 60)
+
+# budget = 20
+
+# print(budget + 10)
+
+# print(20 + 30)
+
+# x = 10
+# y = 20
+
+# z = x * y
+
+# print(z)
+
+# price = 14
+# discount = 0.2 * price
+
+# print(discount)
+
+# Debugging - დებაგინგი არის არასწორი კოდის გამოსწორება.
+
+# 1age = 25
+# last_name = margia'
+# print1(1age - last_name)
+
+
+# age = 25
+
+# last_name = 'Margiani'
+
+# print(age, last_name)
+
+# კოდის დაწერა ანუ 'ქოუდინგი' მოიცავს სამ ეტაპს: 1. კოდის დაწერა   2. კოდის გაშვება და 3. კოდის გასწორება ანუ დებაგინდი
+# Bug - შეცდომა 
+
+
+
+
---
+++ Group 97/Day 02/Home/main.py
@@ -0,0 +1,99 @@
+
+# HOMEWORK
+
+# 1. Sololearn +
+
+# 2. ნასწავლი საკითხები 
+
+# ტექსტური მნიშვნელობის შემცველ მონაცემს STRING ეწოდება. Python-ში 'string' უნდა ჩაისვას ბრჭყალებში.
+# Python-ში string-ისთვის შეგვიძლია გამოვიყენოთ როგორც ერთმაგი ასევე, ორმაგი ბრჭყალები. უნდა გავითვალისწინოთ, რომ თუ გვინდა string-ში ჩავსვათ სიტყვების შემოკლებული ვერსია მაგ: I'll ასეთ შემთხვევაში აუცილებლად ორმაგი ბრჭყალი უნდა გამოვიყენოთ, რათა პროგრამა არ დაიბნეს და შეცდომა არ გამოგვიტანოს ტერმინალში.
+# კომპიუტერულ პროგრამებში კოდი შედგება განცხადებებისგან (statements). განცხადებები არის ინსტრუქციები, რომლებსაც კომპიუტერი მიჰყვება.
+# რიცხვით მონაცემებს - integer არ სჭირდება ბრჭყალები, გარდა იმ შემთხვევისა, როდესაც გვინდა რიცხვით მონაცემს ტექსტური მნიშვნელობა მივანიჭოთ.
+# ცვლადის სახელი გამოიყენება იმის დასადგენად, თუ სად ინახება ინფორმაცია. ცვლადის სახელის გამოძახებით შეგიძლია მიიღო ის მნიშვნელობა, რომელსაც იგი ინახავს.
+# ცვლადის განახლება - reassigning of variable - ერთი და იგივე სახელის მქონე ცვლადების შექმნის შემთხვევაში ტერმინალში/ეკრანზე გამოვა ბოლოს შექმნილი ცვლადის მნიშვნელობა. 
+# case sensitive  - პითონში კოდის წერისას ზუსტად უნდა გადავცეთ პრინტს ცვლადის სახელწოდება თუ გვინდა კონკრეტულად ის გამოიტანოს რა მნიშვნელობაც ცვლადის სახელს შეესაბამება.
+
+# 3. Debugging
+
+# Bug - შეცდომა 
+# კოდის დაწერა ანუ 'ქოუდინგი' მოიცავს სამ ეტაპს: 1. კოდის დაწერა   2. კოდის გაშვება და 3. კოდის გასწორება ანუ დებაგინდი  
+
+# Debugging - დებაგინგი არის არასწორი კოდის გამოსწორება. 
+
+# 4. მაგალითები Debugging -ზე 
+
+#    1color = 'red'                 
+#    print(color) 
+color = 'red'
+print(color)
+
+#   last name = 'Margiani
+#   print(last name)
+last_name = 'Margiani'
+print(last_name)
+
+#   age = 32'
+#   print(age)
+age = 32
+print(age)
+
+#   city / 'Tbilisi'
+#   print(city)
+city = 'Tbilisi'
+print(city)
+
+#   print(last_name + age)
+print(last_name, age)
+
+
+#  5. String/Integer/Variable/Reassigning of variable/Case sensitive 
+
+print('Hello world')
+print('Tamari Margiani')
+print('32')
+print('I am a student')
+print("I'm a student")  
+
+print(5)
+print(25.5)
+print(20 * 5)
+print(56 / 8)
+print(40 // 3)
+
+country = 'Georgia'
+city = 'Tbilisi'
+season = 'Winter'
+season = 'Spring'
+month = 'February'
+month = 'March'
+temperature = -8
+
+print(season + ' ' + month)
+
+
+Academy = 'Goa'
+academy = 'goa'
+ACADEMY = 'GOA'
+
+print(Academy)
+print(academy)
+print(ACADEMY)
+
+
+# 6. ვიდეოს ნახვა + 
+
+# 7. წინადადების შედგენა ცვლადების გამოყენებით
+
+profession = 'Web development'
+verb = 'seems'
+adjective = 'interesting'
+linking_word = 'but' 
+Adjective = 'difficult'
+
+print(profession + ' ' + verb + ' ' + adjective + ' ' + linking_word + ' ' + Adjective)
+
+
+
+
+
+
